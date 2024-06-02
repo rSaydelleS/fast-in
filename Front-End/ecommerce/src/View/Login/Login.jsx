bdb8d2e57dd4aa2725726396699f626fa6b58c56
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-02 11:19:56 -0300

    Começando a criação do form e instalando o react-hook-form

--- Front-End/ecommerce/src/View/Login/Login.jsx
+++ Front-End/ecommerce/src/View/Login/Login.jsx
@@ -1,7 +1,24 @@
+import { useForm } from "react-hook-form";
+
 const Login = () => {
+  const { register, handelSubmit } = useForm();
+
   return (
     <>
-      <h1>Login</h1>
+      <main>
+        <section>
+          <h1>Bem vndo ao fast-in</h1>
+        </section>
+        <section>
+          <form>
+            <p>Email:</p>
+            <input name="email" placeholder="Digite seu email..." />
+            <p>Senha:</p>
+            <input name="senha" placeholder="******" />
+            <button type="submit"></button>
+          </form>
+        </section>
+      </main>
     </>
   );
 };
