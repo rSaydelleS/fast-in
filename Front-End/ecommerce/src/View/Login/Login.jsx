a0116d0b428034c690ff49644802a278e9e906aa
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-03 20:27:19 -0300

    Finalizando a config do tailwind e estilizando o login

--- Front-End/ecommerce/src/View/Login/Login.jsx
+++ Front-End/ecommerce/src/View/Login/Login.jsx
@@ -8,32 +8,51 @@ const Login = () => {
   } = useForm();
   return (
     <>
-      <main className="">
-        <section>
-          <h1>Bem vndo ao fast-in</h1>
-        </section>
-        <section>
-          <form
-            onSubmit={handleSubmit((data) => {
-              console.log(data);
-            })}
-          >
-            <label>Email:</label>
-            <input
-              type="text"
-              placeholder="Digite seu email"
-              {...register("Email", { required: true })}
-            />
-            <label>Senha:</label>
-            <input
-              type="password"
-              placeholder="******"
-              {...register("Senha", { required: true })}
-            />
-            {errors.Senha && <span>Email ou senha não eocntrados</span>}
-            <button type="submit">Entrar!</button>
-          </form>
-        </section>
+      <main className="flex flex-col justify-center items-center gap-4 bg-gray-300 w-screen h-screen">
+        <div className="p-10 bg-slate-200 shadow-xl shadow-black rounded-xl">
+          <section className="text-3xl items-start font-bold">
+            <h1>Bem vndo ao fast-in</h1>
+          </section>
+          <section>
+            <form
+              onSubmit={handleSubmit((data) => {
+                console.log(data);
+              })}
+              className="flex flex-col items-start gap-8 font-semibold"
+            >
+              <label className="text-2xl">Email:</label>
+              <input
+                type="text"
+                placeholder="Digite seu email"
+                {...register("Email", { required: true })}
+                className="px-4 py-2 rounded-xl text-xl"
+              />
+              <label className="text-2xl">Senha:</label>
+              <input
+                type="password"
+                placeholder="******"
+                {...register("Senha", { required: true })}
+                className="px-4 py-2 rounded-xl text-xl"
+              />
+              {errors.Senha && (
+                <span className="text-red-500 text-xl border-b-2 border-red-500">
+                  Email ou senha não cadastrados
+                </span>
+              )}
+              <div className="flex flex-row justify-around items-end w-full">
+                <a className="w-40 cursor-pointer">
+                  Não possui uma conta? Clique aqui para criar
+                </a>
+                <button
+                  className=" px-5 py-2 rounded-xl bg-green-400 text-lg"
+                  type="submit"
+                >
+                  Entrar!
+                </button>
+              </div>
+            </form>
+          </section>
+        </div>
       </main>
     </>
   );
