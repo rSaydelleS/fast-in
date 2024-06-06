01b39fb7db5610395a42f776bb1e81296ff14382
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-06 19:55:20 -0300

    Estilizando o ItemCard

--- Front-End/ecommerce/src/components/Card/Item_Card.jsx
+++ Front-End/ecommerce/src/components/Card/Item_Card.jsx
@@ -2,18 +2,20 @@ const Item_Card = () => {
   return (
     <>
       <main>
-        <section>
+        <section className="w-96 h-96 bg-white rounded-xl font-bold flex justify-center items-center ">
           <h1>Imagem</h1>
         </section>
-        <section>
+        <section className="text-xl py-3">
           <p>Descrição do produto</p>
         </section>
-        <section>
+        <section className="flex flex-row justify-between font-bold">
           <section>
-            <h3>Preço</h3>
+            <h3>R$: Preço</h3>
           </section>
           <section>
-            <button>Adicionar ao carrinho</button>
+            <button className="bg-sky-300 py-2 px-2 hover:rounded-lg hover:bg-sky-500 transition-all duration-500">
+              Adicionar ao carrinho {">"}
+            </button>
           </section>
         </section>
       </main>
