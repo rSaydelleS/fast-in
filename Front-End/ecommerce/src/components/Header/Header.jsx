2d5f76a41dbe7167130a8d5a68fd742ac3948560
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-10 20:06:06 -0300

    Estruturando o Header

--- Front-End/ecommerce/src/components/Header/Header.jsx
+++ Front-End/ecommerce/src/components/Header/Header.jsx
@@ -1,4 +1,21 @@
+import { useState } from "react";
+
 const Header = () => {
+  const [about] = useState([
+    {
+      id: 1,
+      description: "Cadastrar produto",
+    },
+    {
+      id: 2,
+      description: "Sobre nós",
+    },
+    {
+      id: 3,
+      description: "centra de suporte",
+    },
+  ]);
+
   return (
     <>
       <header>
@@ -6,6 +23,16 @@ const Header = () => {
           <div className="w-[150px] h-[150px]">
             <img src="../../../public/fast-in-logo.png" alt="Logo" />
           </div>
+          <section>
+            <ul>
+              {about.map((item) => {
+                return <li key={item.id}>{item.description}</li>;
+              })}
+            </ul>
+          </section>
+          <section>
+            <p>usuário</p>
+          </section>
         </main>
       </header>
     </>
