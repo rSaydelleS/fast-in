a75144d6b131ea163a749c4bcfabf0c19d61ff00
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-11 19:54:38 -0300

    Adicioando a rota de cadastro

--- Front-End/ecommerce/src/main.jsx
+++ Front-End/ecommerce/src/main.jsx
@@ -2,22 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Login from "../src/View/Login/Login.jsx";
 import App from "./App.jsx";
+import Form from "../src/View/Form/cadastroForm.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const Router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />,
   },
   {
-    path: "/Home",
+    path: "/home",
     element: <App />,
+  },
+  {
+    path: "/cadastrar",
+    element: <Form />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
