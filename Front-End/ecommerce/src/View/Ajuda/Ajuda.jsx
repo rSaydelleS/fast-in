66cb7d7bfcfe50d99b7f58d51512d7d9decc8e69
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-13 20:33:28 -0300

    Estilizando a  Cnetral de Ajuda

--- Front-End/ecommerce/src/View/Ajuda/Ajuda.jsx
+++ Front-End/ecommerce/src/View/Ajuda/Ajuda.jsx
@@ -2,11 +2,13 @@ const Ajuda = () => {
   return (
     <>
       <main>
-        <section>
-          <p>
-            Página ficticia, nenhum dado é verificado apenas para poder simular
-            um ecommerce e as etapas de percurso do usuário!
-          </p>
+        <section className="flex justify-center items-center w-screen h-screen bg-neutral-100">
+          <section className="bg-white shadow-lg shadow-slate-500 rounded-md p-10 text-2xl m-10">
+            <p className="max-w-96">
+              Site ficticio, nenhum dado é verificado ou usado apenas para poder
+              simular um ecommerce e as etapas de percurso do usuário!
+            </p>
+          </section>
         </section>
       </main>
     </>
