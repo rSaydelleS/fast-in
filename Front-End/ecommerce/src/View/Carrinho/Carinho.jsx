362a45e6663735aa938e9b948f57de5cdd9d9114
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-13 21:13:38 -0300

    Estilizando o Carrinho

--- Front-End/ecommerce/src/View/Carrinho/Carinho.jsx
+++ Front-End/ecommerce/src/View/Carrinho/Carinho.jsx
@@ -4,9 +4,9 @@ const Carrinho = () => {
   return (
     <>
       <main>
-        <section>
-          <h1>Seus pedidos</h1>
-          <section>
+        <section className="flex flex-col justify-start items-start my-8 mx-10">
+          <h1 className="font-bold text-3xl">Seus pedidos</h1>
+          <section className="flex flex-col text-justify bg-white shadow-lg shadow-slate-500 rounded-md text-2xl">
             <CarrinhoItem />
           </section>
         </section>
