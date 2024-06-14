fa64e596a836a3c7525d56c9be05776a7c870b2a
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-13 21:18:41 -0300

    Adicionando mais estilos ao Carrinho

--- Front-End/ecommerce/src/View/Carrinho/Carinho.jsx
+++ Front-End/ecommerce/src/View/Carrinho/Carinho.jsx
@@ -3,10 +3,10 @@ import CarrinhoItem from "../../components/Carrinho_Item/Carrinho_Item";
 const Carrinho = () => {
   return (
     <>
-      <main>
+      <main className="bg-neutral-100 w-screen h-screen">
         <section className="flex flex-col justify-start items-start my-8 mx-10">
           <h1 className="font-bold text-3xl">Seus pedidos</h1>
-          <section className="flex flex-col text-justify bg-white shadow-lg shadow-slate-500 rounded-md text-2xl">
+          <section className="flex flex-col text-justify w-full bg-white shadow-lg shadow-slate-500 rounded-md text-2xl">
             <CarrinhoItem />
           </section>
         </section>
