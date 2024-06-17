edaeceee28bd85829451ff1c8a69e54629fd707f
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-17 20:50:38 -0300

    Crinado o model de produto do mongoose

---
+++ Back-End/ecommerce/src/model/prodModel.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require("mongoose");
+
+const mongoose = require(mongoose);
+const conn = require("../connection/bdconect");
+
+conn();
+const prodSchema = mongoose.Schema(
+  {
+    nomeProduto: String,
+    precoProduto: Number,
+    imgProduto: String,
+  },
+  {
+    timestemp: true,
+  }
+);
+
+const prodModel = mongoose.model("produto", prodSchema);
+module.exports = prodModel;
