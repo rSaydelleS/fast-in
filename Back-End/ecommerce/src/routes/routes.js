c482c6ec488e359586dea88fc7d24152c2368cfc
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-18 19:14:49 -0300

    Adicionando o try catch

--- Back-End/ecommerce/src/routes/routes.js
+++ Back-End/ecommerce/src/routes/routes.js
@@ -3,24 +3,32 @@ const router = express.Router();
 const prodModel = require("../model/prodModel");
 
 router.get("/home", async (req, res) => {
-  let response = await prodModel.find({});
-  res.json({
-    status: "ok",
-    data: response,
-  });
+  try {
+    let response = await prodModel.find({});
+    res.json({
+      status: "ok",
+      data: response,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/novo", async (req, res) => {
-  let data = await prodModel.create({
-    nomeProduto: req.body.produto,
-    precoProduto: req.body.preco,
-    imgProduto: req.body.imagem,
-  });
+  try {
+    let data = await prodModel.create({
+      nomeProduto: req.body.produto,
+      precoProduto: req.body.preco,
+      imgProduto: req.body.imagem,
+    });
 
-  res.json({
-    status: "ok",
-    data: data,
-  });
+    res.json({
+      status: "ok",
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
