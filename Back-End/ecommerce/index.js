17772d77338624d1bda9680f5be0601ff7c0fa59
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-18 21:13:31 -0300

    Adicionando mais configurações no index.js

--- Back-End/ecommerce/index.js
+++ Back-End/ecommerce/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5173;
 const routes = require("./src/routes/routes");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`servidor operacional na porta: ${port}`);
