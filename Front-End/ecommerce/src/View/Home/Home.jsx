8d5112c21cd680360f721eb44cfab03592b507d8
rSaydelleS <rafael.ppuerari@gmail.com>
2024-06-28 20:03:16 -0300

    Adicionando a resposividade na home

--- Front-End/ecommerce/src/View/Home/Home.jsx
+++ Front-End/ecommerce/src/View/Home/Home.jsx
@@ -19,12 +19,12 @@ const Home = () => {
   return (
     <>
       <main className="w-screen h-screen bg-neutral-100 flex flex-col justify-between items-center text-justify ">
-        <section className="my-12 grid grid-cols-3 grid-rows-subgrid gap-16">
+        <section className="my-12 max-sm:flex max-sm:flex-col grid grid-cols-3 grid-rows-subgrid gap-16">
           {dados.map((item) => {
             return <Item_Card key={item._id} dados={item} />;
           })}
         </section>
-        <section className="bg-slate-600 text-white w-full p-4">
+        <section className="bg-slate-600 text-white w-screen p-4">
           <footer>
             <h1>E commerce ficticio</h1>
           </footer>
